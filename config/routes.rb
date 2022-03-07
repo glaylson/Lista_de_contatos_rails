34564ab6aede88f3d97937bf4dd468dc17e289d2
glaylson <glaylsonflf@gmail.com>
2022-03-07 18:28:40 -0300

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :contacts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #root to: "static_pages#index"
+  
   root to: 'static_pages#index'  
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'  
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+
+  resources :contacts
+  resources :users, only: [:new, :create, :show] 
 
 end
